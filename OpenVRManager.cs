40bdac50510cf00479c8e35f625f0ac9748d7e12
jangxx <janscheiper95@gmail.com>
2022-03-21 23:46:08 +0100

    initial commit

---
+++ OpenVRManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valve.VR;
+
+namespace VRC_OSC_ExternallyTrackedObject
+{
+    internal class OpenVRManager
+    {
+        private CVRSystem cVR;
+
+        OpenVRManager()
+        {
+
+        }
+
+        public void InitBackground()
+        {
+            if (cVR != null)
+            {
+                Shutdown();
+            }
+
+            EVRInitError error = EVRInitError.None;
+            cVR = OpenVR.Init(ref error, EVRApplicationType.VRApplication_Background);
+        }
+
+        public void InitOverlay()
+        {
+            if (cVR != null)
+            {
+                Shutdown();
+            }
+
+            EVRInitError error = EVRInitError.None;
+            cVR = OpenVR.Init(ref error, EVRApplicationType.VRApplication_Overlay);
+        }
+
+        public void Shutdown()
+        {
+            if (cVR != null)
+            {
+                OpenVR.Shutdown();
+            }
+        }
+    }
+}
