277634123a6dba3c57934645f4e88f505d4966af
jangxx <janscheiper95@gmail.com>
2022-03-30 02:25:56 +0200

    more work on the gui, started on the ovr manager

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,119 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VRC_OSC_ExternallyTrackedObject
 {
+    internal class AvatarParams
+    {
+        public string Activate { get; set; } = "OSCTrackingActivate";
+        public string PositionX { get; set; } = "OSCTrackingPosX";
+        public string PositionY { get; set; } = "OSCTrackingPosY";
+        public string PositionZ { get; set; } = "OSCTrackingPosZ";
+        public string RotationX { get; set; } = "OSCTrackingRotX";
+        public string RotationY { get; set; } = "OSCTrackingRotY";
+        public string RotationZ { get; set; } = "OSCTrackingRotZ";
+    }
+
+    internal class AvatarCalibration
+    {
+        public double Scale { get; set; } = 1;
+        public double TranslationX { get; set; } = 0;
+        public double TranslationY { get; set; } = 0;
+        public double TranslationZ { get; set; } = 0;
+        public double RotationX { get; set; } = 0;
+        public double RotationY { get; set; } = 0;
+        public double RotationZ { get; set; } = 0;
+    }
+
+    internal class AvatarConfig
+    {
+        public string Name { get; set; } = "";
+        public AvatarParams Parameters { get; set; } = new AvatarParams();
+        public AvatarCalibration Calibration { get; set; } = new AvatarCalibration();
+    }
+
+    internal class Configuration
+    {
+        public bool Autostart { get; set; } = false;
+        public string OscInputAddress { get; set; } = "127.0.0.1:9001";
+        public string OscOutputAddress { get; set; } = "127.0.0.1:9000";
+        public string? ControllerSerial { get; set; }
+        public string? TrackerSerial { get; set; }
+        public Dictionary<string, AvatarConfig> Avatars { get; set; } = new Dictionary<string, AvatarConfig>();
+    }
+
+    internal class AvatarListItem
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string DisplayName
+        {
+            get { return Name + " (" + Id + ")"; }
+        }
+
+        public AvatarListItem(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    //public class MainWindowProperties : INotifyPropertyChanged
+    //{
+    //    public event PropertyChangedEventHandler PropertyChanged;
+    //    protected virtual void OnPropertyChanged(string propertyName)
+    //    {
+    //        PropertyChangedEventHandler handler = PropertyChanged;
+    //        if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+    //    }
+    //    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    //    {
+    //        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+    //        field = value;
+    //        OnPropertyChanged(propertyName);
+    //        return true;
+    //    }
+
+    //    private bool allInputsEnabled;
+    //    public bool AllInputsEnabled
+    //    {
+    //        get { return allInputsEnabled; }
+    //        set { SetField(ref allInputsEnabled, value); }
+    //    }
+    //}
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Configuration CurrentConfig = new Configuration();
+        private ObservableCollection<AvatarListItem> AvatarList = new ObservableCollection<AvatarListItem>();
+        //public MainWindowProperties DisplayProperties { get; set; }
+        private OpenVRManager OpenVRManager = new OpenVRManager();
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            AvatarList.Add(new AvatarListItem("ididiid", "namenamenae"));
+
+            AvatarDropdown.ItemsSource = AvatarList;
+            AvatarListBox.ItemsSource = AvatarList;
+
+            //this.DisplayProperties = new MainWindowProperties();
+            //this.DisplayProperties.AllInputsEnabled = true;
+
+            if (AvatarList.Count > 0)
+            {
+                AvatarDropdown.SelectedIndex = 0;
+            }
+
+            this.OpenVRManager.InitOverlay();
         }
 
         private void Btn_saveConfig(object sender, RoutedEventArgs e)
@@ -31,6 +122,163 @@ namespace VRC_OSC_ExternallyTrackedObject
 
         private void Btn_openConfig(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void Btn_addAvatar(object sender, RoutedEventArgs e)
+        {
+            if (NewAvatarId.InputText == "" || NewAvatarId.InputText == null)
+            {
+                MessageBox.Show("The id field can not be empty", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (NewAvatarName.InputText == "" || NewAvatarName.InputText == null)
+            {
+                MessageBox.Show("The name field can not be empty", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var avatarId = NewAvatarId.InputText;
+            var avatarName = NewAvatarName.InputText;
+
+            var config = new AvatarConfig();
+            config.Name = avatarName;
+
+            CurrentConfig.Avatars.Add(avatarId, config);
+
+            var listItem = new AvatarListItem(avatarId, avatarName);
+            AvatarList.Add(listItem);
+
+            NewAvatarId.InputText = "";
+            NewAvatarName.InputText = "";
+
+            if (AvatarList.Count == 1)
+            {
+                AvatarDropdown.SelectedIndex = 0;
+            }
+        }
+
+        private void Btn_deleteAvatar(object sender, RoutedEventArgs e)
+        {
+            if (AvatarListBox.SelectedItem == null) return;
+
+            CurrentConfig.Avatars.Remove(((AvatarListItem)AvatarListBox.SelectedItem).Id);
+            AvatarList.Remove((AvatarListItem)AvatarListBox.SelectedItem);
+        }
+
+        private void AvatarDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AvatarDropdown.SelectedItem == null)
+            {
+                ClearAndDisableAllInputs();
+                return;
+            }
+
+            var currentAvatarId = ((AvatarListItem)AvatarDropdown.SelectedItem).Id;
+        }
+
+        private void ClearAndDisableAllInputs()
+        {
+            ParamTrigger.InputText = "";
+            ParamTrigger.IsEnabled = false;
+            ParamPosX.InputText = "";
+            ParamPosX.IsEnabled = false;
+            ParamPosY.InputText = "";
+            ParamPosY.IsEnabled = false;
+            ParamPosZ.InputText = "";
+            ParamPosZ.IsEnabled = false;
+            ParamRotX.InputText = "";
+            ParamRotX.IsEnabled = false;
+            ParamRotY.InputText = "";
+            ParamRotY.IsEnabled = false;
+            ParamRotZ.InputText = "";
+            ParamRotZ.IsEnabled = false;
+            CalibrationScale.InputText = "";
+            CalibrationScale.IsEnabled = false;
+            CalibrationPosX.InputText = "";
+            CalibrationPosX.IsEnabled = false;
+            CalibrationPosY.InputText = "";
+            CalibrationPosY.IsEnabled = false;
+            CalibrationPosZ.InputText = "";
+            CalibrationPosZ.IsEnabled = false;
+            CalibrationRotX.InputText = "";
+            CalibrationRotX.IsEnabled = false;
+            CalibrationRotY.InputText = "";
+            CalibrationRotY.IsEnabled = false;
+            CalibrationRotZ.InputText = "";
+            CalibrationRotZ.IsEnabled = false;
+
+            StartTrackingButton.IsEnabled = false;
+            StartCalibrationButton.IsEnabled = false;
+        }
+
+        private void FillAndEnableAllInputs(AvatarConfig config)
+        {
+            ParamTrigger.InputText = config.Parameters.Activate;
+            ParamTrigger.IsEnabled= true;
+            ParamPosX.InputText = config.Parameters.PositionX;
+            ParamPosX.IsEnabled= true;
+            ParamPosY.InputText = config.Parameters.PositionY;
+            ParamPosY.IsEnabled= true;
+            ParamPosZ.InputText = config.Parameters.PositionZ;
+            ParamPosZ.IsEnabled= true;
+            ParamRotX.InputText = config.Parameters.RotationX;
+            ParamRotX.IsEnabled= true;
+            ParamRotY.InputText = config.Parameters.RotationY;
+            ParamRotY.IsEnabled= true;
+            ParamRotZ.InputText = config.Parameters.RotationZ;
+            ParamRotZ.IsEnabled= true;
+            CalibrationScale.InputText = config.Calibration.Scale.ToString();
+            CalibrationScale.IsEnabled= true;
+            CalibrationPosX.InputText = config.Calibration.TranslationX.ToString();
+            CalibrationPosX.IsEnabled= true;
+            CalibrationPosY.InputText = config.Calibration.TranslationY.ToString();
+            CalibrationPosY.IsEnabled= true;
+            CalibrationPosZ.InputText = config.Calibration.TranslationZ.ToString();
+            CalibrationPosZ.IsEnabled= true;
+            CalibrationRotX.InputText = config.Calibration.RotationX.ToString();
+            CalibrationRotX.IsEnabled= true;
+            CalibrationRotY.InputText = config.Calibration.RotationY.ToString();
+            CalibrationRotY.IsEnabled= true;
+            CalibrationRotZ.InputText = config.Calibration.RotationZ.ToString();
+            CalibrationRotZ.IsEnabled= true;
+
+            StartTrackingButton.IsEnabled= true;
+            StartCalibrationButton.IsEnabled= true;
+        }
+
+        private void StartCalibrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            //this.OpenVRManager.StartCalibrationThread();
+        }
+
+        private void StartTrackingButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MainWindowName_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (!this.OpenVRManager.IsCalibrationThreadRunning()) return;
+
+            if (e.Key == System.Windows.Input.Key.Up ||
+                e.Key == System.Windows.Input.Key.Down ||
+                e.Key == System.Windows.Input.Key.Left ||
+                e.Key == System.Windows.Input.Key.Right)
+            {
+                this.OpenVRManager.InjectKeyPress(e.Key);
+            }
+
+            e.Handled = true;
+        }
+
+        private void TrackerDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ControllerDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
--- OpenVRManager.cs
+++ OpenVRManager.cs
@@ -1,19 +1,119 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using Valve.VR;
 
 namespace VRC_OSC_ExternallyTrackedObject
 {
+    internal class OVRException : Exception {
+        public OVRException(string message) : base(message) { }
+    }
+
+    internal class TrackedObjectListEntry
+    {
+        public uint Index { get; set; }
+        public string Name { get; set; }
+
+    }
+
     internal class OpenVRManager
     {
-        private CVRSystem cVR;
+        private CVRSystem? cVR;
+        private BlockingCollection<Key> InputKeyQueue = new BlockingCollection<Key>();
+        private CancellationTokenSource CancelTokenSource = new CancellationTokenSource();
+        private Thread? currentThread = null;
+        private bool calibrationThreadRunning = false;
+        private Dictionary<string, uint> Controllers = new Dictionary<string, uint>();
+        private Dictionary<string, uint> Trackers = new Dictionary<string, uint>();
 
-        OpenVRManager()
+        public OpenVRManager()
         {
 
+        }
+
+        public bool IsCalibrationThreadRunning()
+        {
+            return calibrationThreadRunning;
+        }
+
+        public void InjectKeyPress(Key key)
+        {
+            InputKeyQueue.Add(key);
+        }
+
+        public void UpdateControllers()
+        {
+            if (cVR == null) return;
+
+            Controllers.Clear();
+            Trackers.Clear();
+
+            // this is not optimal, ideally we could put everything in the same array but I don't think c# slices actually support that?
+            uint[] controllerIds = new uint[OpenVR.k_unMaxTrackedDeviceCount];
+            uint[] trackerIds = new uint[OpenVR.k_unMaxTrackedDeviceCount];
+            uint noOfControllers = OpenVR.System.GetSortedTrackedDeviceIndicesOfClass(ETrackedDeviceClass.Controller, controllerIds, OpenVR.k_unTrackedDeviceIndex_Hmd);
+            uint noOfTrackers = OpenVR.System.GetSortedTrackedDeviceIndicesOfClass(ETrackedDeviceClass.GenericTracker, trackerIds, OpenVR.k_unTrackedDeviceIndex_Hmd);
+
+            try
+            {
+                for (uint i = 0; i < noOfControllers; i++)
+                {
+                    uint id = controllerIds[i];
+                    Controllers.Add(GetStringTrackedDeviceProperty(id, ETrackedDeviceProperty.Prop_SerialNumber_String), id);
+                }
+                for (uint i = 0; i < noOfTrackers; i++)
+                {
+                    uint id = trackerIds[i];
+                    Trackers.Add(GetStringTrackedDeviceProperty(id, ETrackedDeviceProperty.Prop_SerialNumber_String), id);
+                }
+            }
+            catch (OVRException e)
+            {
+                MessageBox.Show("Updating controllers and trackers encountered an unexpected OpenVR error: " + e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public List<string> GetControllers()
+        {
+            return Controllers.Keys.ToList();
+        }
+
+        public List<string> GetTrackers()
+        {
+            return Trackers.Keys.ToList();
+        }
+
+        private string GetStringTrackedDeviceProperty(uint deviceIndex, ETrackedDeviceProperty prop)
+        {
+            ETrackedPropertyError err = ETrackedPropertyError.TrackedProp_Success;
+
+            StringBuilder sb = new StringBuilder(128);
+            uint propLen = OpenVR.System.GetStringTrackedDeviceProperty(deviceIndex, prop, sb, (uint)sb.Capacity, ref err);
+            if (err == ETrackedPropertyError.TrackedProp_Success)
+            {
+                return sb.ToString();
+            }
+            else if (err == ETrackedPropertyError.TrackedProp_BufferTooSmall)
+            {
+                // try again with larger buffer
+                sb.Capacity = (int)propLen;
+                propLen = OpenVR.System.GetStringTrackedDeviceProperty(deviceIndex, prop, sb, (uint)sb.Capacity, ref err);
+            }
+
+            if (err != ETrackedPropertyError.TrackedProp_Success)
+            {
+                throw new OVRException(err.ToString());
+            }
+
+            return sb.ToString();
         }
 
         public void InitBackground()
@@ -43,7 +143,94 @@ namespace VRC_OSC_ExternallyTrackedObject
             if (cVR != null)
             {
                 OpenVR.Shutdown();
+                cVR = null;
             }
+        }
+
+        public void StartTrackingThread()
+        {
+            if (currentThread != null) return;
+        }
+
+        public void TrackingThreadMain()
+        {
+
+        }
+
+        public void StartCalibrationThread(string controllerSn)
+        {
+            if (currentThread != null) return;
+
+            if (!Controllers.ContainsKey(controllerSn))
+            {
+                MessageBox.Show("The controller " + controllerSn + " does not exist", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            uint controllerHandle = Controllers[controllerSn];
+
+            this.currentThread = new Thread(() => CalibrationThreadMain(controllerHandle));
+            this.currentThread.Name = "CalibrationThread";
+            this.currentThread.Start();
+            this.calibrationThreadRunning = true;
+        }
+
+        public void CalibrationThreadMain(uint controllerHandle)
+        {
+            ulong OverlayXHandle = OpenVR.k_ulOverlayHandleInvalid;
+            ulong OverlayYHandle = OpenVR.k_ulOverlayHandleInvalid;
+            ulong OverlayZHandle = OpenVR.k_ulOverlayHandleInvalid;
+            Key nextKey;
+
+            HmdMatrix34_t OverlayXMat = new HmdMatrix34_t();
+
+            try
+            {
+                ThrowOVRError(OpenVR.Overlay.CreateOverlay("com.jangxx.vrc_osc_calibrate_x", "OSC Debug X Plane", ref OverlayXHandle));
+                ThrowOVRError(OpenVR.Overlay.SetOverlayWidthInMeters(OverlayXHandle, 0.1f));
+                ThrowOVRError(OpenVR.Overlay.SetOverlayTransformTrackedDeviceRelative(OverlayXHandle, controllerHandle, ref OverlayXMat));
+                ThrowOVRError(OpenVR.Overlay.ShowOverlay(OverlayXHandle));
+                ThrowOVRError(OpenVR.Overlay.SetOverlayFromFile(OverlayXHandle, Path.GetFullPath("img\\debug overlay X.png")));
+
+                while (InputKeyQueue.TryTake(out nextKey, -1, CancelTokenSource.Token))
+                {
+
+                }
+            }
+            catch(OperationCanceledException)
+            {
+                Debug.WriteLine("Calibration thread exited");
+            }
+            catch (OVRException e)
+            {
+                MessageBox.Show("Calibration encountered an unexpected OpenVR error: " + e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Calibration thread encountered an unexpected error: " + e.ToString() + " (" + e.Message + ")", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            //finally
+            //{
+            //    Shutdown();
+            //}
+        }
+
+        private void ThrowOVRError(EVROverlayError err)
+        {
+            if (err != EVROverlayError.None)
+            {
+                throw new OVRException(err.ToString());
+            }
+        }
+
+        public void StopThread()
+        {
+            if (currentThread == null) return;
+
+            CancelTokenSource.Cancel();
+            currentThread.Join();
+            currentThread = null;
+            this.calibrationThreadRunning = false;
         }
     }
 }
